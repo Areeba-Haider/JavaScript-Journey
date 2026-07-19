dbda4acb8cde47b5e7ef701fe4cda142285ff610
Areeba-Haider <areebahaider108@gmail.com>
2026-07-19 12:32:10 +0500

    Sort and Reverse method

---
+++ Day-7/sorting.js
@@ -0,0 +1,18 @@
+
+
+let names = ["James", "Alicia", "Fatiha", "Maria", "Bert"];
+names.sort();
+
+console.log(names);
+
+let ages = [18, 72, 33, 56, 40];
+ages.sort();
+
+console.log(ages);
+
+let num =[ 10 ,9,16 ,4 ,3 ,100, 12 ];
+num.sort();
+console.log(num);
+
+
+
